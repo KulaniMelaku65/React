f0f81710d88006c706106f601480fb53072e2e81
KulaniMelaku65 <kulanimelaku@gmail.com>
2021-10-01 23:58:54 +0300

    second commit

--- server/index.js
+++ server/index.js
@@ -5,8 +5,14 @@ const PORT= process.env.PORT || 3001;
 
 const app=express();
 
+app.use(express.static(path.resolve(_dirname, '../todoclient/build')));
+
 app.get("/api", (req, res)=> {
     res.json({message:"Hello from server!"});
+});
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, '../todoclient/build', 'index.html'));
 });
 
 
